598c50116cb5a0708bf37221271a41c2f5ce9dab
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-02 15:28:30 +0600

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="bg-blue-400 text-white">button</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Index</h1>
+  <Button></Button>
+</template>
+
+<script>
+export default {
+  name: "index",
+};
+</script>
